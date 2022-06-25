77b2e3e399d83aa1b50ee11164e46bf47e016d51
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-06-25 17:25:28 +0300

    Add files via upload

---
+++ Src Code/InfiniteLoopMusicPygame.py
@@ -0,0 +1,11 @@
+import threading
+import pygame
+
+
+import threading
+import pygame
+pygame.mixer.init();
+pygame.mixer_music.load("C:\Temp\MRisingMusic.mp3");
+pygame.mixer_music.play(-1);
+x = threading.Event();
+x.wait();
